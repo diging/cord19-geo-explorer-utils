732dfde5310a04d188bdec14ae0d7a2f659b1d51
Julia Damerow <julia.damerow@gmail.com>
2020-05-12 09:00:56 -0400

    adjustments to get all coordinates

--- wikipedia/ingest_elastic.py
+++ wikipedia/ingest_elastic.py
@@ -28,7 +28,7 @@ def create_index(es_object, index_name):
 def store_record(elastic_object, record, index_name, record_counter=1):
     try:
         outcome = elastic_object.index(index=index_name, doc_type='wikientry', body=record, timeout="600s")
-        if record_counter%50==0:
+        if record_counter%3000==0:
             print("Waiting.... ")
             time.sleep(3)
     except Exception as ex:
@@ -44,14 +44,17 @@ class PageHandler ( xml.sax.ContentHandler):
     self.counter = 0
     self.current_element = None
     self.title = None
+    self.wiki_id = ""
     self.is_page = False
     self.is_title = False
+    self.is_revision = False
+    self.is_id = False
     self.current_doc = {}
     self.current_text = ""
     self.new_records = True
     self.use_wait = use_wait
 
-  def startElement( self, name, attrs):
+  def startElement(self, name, attrs):
     if name == "page":
         self.is_page = True
     self.current_element = name
@@ -59,8 +62,12 @@ class PageHandler ( xml.sax.ContentHandler):
         self.is_title = True
     if name == "text" and self.is_page:
         self.counter += 1
+    if name == "id":
+        self.is_id = True
+    if name == "revision":
+        self.is_revision = True
 
-  def endElement( self, name):
+  def endElement(self, name):
     if name == "page":
         if self.new_records:
             self.submit_entry()
@@ -74,9 +81,14 @@ class PageHandler ( xml.sax.ContentHandler):
         self.current_doc = {}
         self.current_element = None
         self.title = None
+        self.wiki_id = ""
         self.current_text = ""
     if name == "title":
         self.is_title = False
+    if name == "id":
+        self.is_id = False
+    if name == "revision":
+        self.is_revision = False
 
   def characters(self, data):
     if self.current_element == "title" and self.is_page and self.is_title:
@@ -85,17 +97,24 @@ class PageHandler ( xml.sax.ContentHandler):
         self.current_doc["title_keyword"] = data
     if self.current_element == "text" and self.is_page:
         self.current_text = self.current_text + data
+    if self.current_element == "id" and not self.is_revision:
+        self.wiki_id = self.wiki_id + data
 
   def submit_entry(self):
       lines = []
       self.current_doc["categories"] = []
       self.current_doc["complete_text"] = self.current_text
+      self.current_doc["wiki_id"] = self.wiki_id
       for line in self.current_text.splitlines():
           m = re.search(r'\|\s*coordinates\s*=\s*\{\{(.*?)\}\}', line)
+          m2 = re.search(r'^\s*\{\{(coord\|.+?)\}\}', line.lower())
           if line.strip().startswith("{{short description|"):
               self.current_doc["short_description"] = line.strip()[len("{{short description|"):-2]
           elif m and m.group(1):
               self.current_doc["coordinates"] = m.group(1)
+              print("Coordinates: " + self.current_doc["coordinates"])
+          elif m2 and m2.group(1):
+              self.current_doc["coordinates"] = m2.group(1)
               print("Coordinates: " + self.current_doc["coordinates"])
           if line.strip().startswith("{{") or line.strip().startswith("|") or line.strip().startswith("*"):
               continue
